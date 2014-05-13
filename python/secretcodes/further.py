368f973a99363d49cc80af8edff8cd91aedeb015
Laurence Roberts <lsjroberts@gmail.com>
2014-05-13 12:55:42 +0100

    Added further examples and cipher image

---
+++ python/secretcodes/further.py
@@ -0,0 +1,13 @@
+# Here are some secret messages to try and decode!
+
+
+message = "n frperg clguba"
+secret = 13
+
+
+message = "yj tjp gdfz xjyz xgpw?"
+secret = 21
+
+
+message = "axkxcb jan hxda oarnwmb"
+secret = 9
